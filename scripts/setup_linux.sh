2056e4472040954df511dee00b8609552ab041de
Krzysztof Stopa <stopa.krzysztof.k@gmail.com>
2017-09-03 16:27:15 +0200

    Added license and authorship information.

--- scripts/setup_linux.sh
+++ scripts/setup_linux.sh
@@ -1,3 +1,24 @@
+#!/usr/bin/env bash
+
+# Copyright 2017 Krzysztof Stopa (stopa.krzysztof.k@gmail.com)
+
+# This file is part of Copernicus Atmosphere Monitoring Service (CAMS) downloading and
+# processing tools (CAMS tools).
+
+# CAMS tools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or any later
+# version.
+
+# CAMS tools is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with CAMS tools.  If not, see <http://www.gnu.org/licenses/>.
+
+
 
 # Install WGRIB2
 
