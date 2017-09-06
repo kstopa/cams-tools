97377a173eacdbc261fd33168aee2478498ffd7f
Krzysztof Stopa <stopa.krzysztof.k@gmail.com>
2017-09-06 07:52:13 +0200

    Added processing options with wgrib2

--- README.md
+++ README.md
@@ -27,11 +27,12 @@ requests library installed. So just run:
 ### GRIB2 conversion option installation (Optional)
 
 To work with archived packages (available only in GRIB2 format) it is
-useful to convert this data to formats like NetCDF or CSV that are
-supported by the majority of GIS software and tools (like GDAL or QGIS).
-To add support for GRIB2 format conversion you must to have
+useful to convert this data to formats like NetCDF, CSV or ASCII grid
+that are supported by the majority of GIS software and tools (like GDAL
+or QGIS). To add support for GRIB2 format conversion you must to have
 [NOAA wgrib2 tool](http://www.cpc.ncep.noaa.gov/products/wesley/wgrib2/),
 an utility to read and write grib2 files.
+
 To install the tool just run the setup script for your OS (by now only
 Linux and MacOS available (feel free to add Windows script)):
 
@@ -41,11 +42,49 @@ Linux and MacOS available (feel free to add Windows script)):
 
     ./scripts/setup_macos.sh
 
+For some tricks about wgrib2 tool usage check
+[this link](http://www.ftp.cpc.ncep.noaa.gov/wd51we/wgrib2/tricks.wgrib2)
+
 ## Usage
 
 To be improved. Just type:
 
+### Downloading data
+
+#### CLI usage
+
     python3 cams.py -h
+
+#### API usage
+
+
+### Converting with WGRIB2
+
+If you have installed wgrib2 tool in your system the tool can uses it
+to convert CAMS GRIB2 data to other formats by using wgib2 tool.
+Currently wgrib2 supports:
+
+- netcdf: write data in netcdf v3 format (or v4 if wgrib2 is compiled with this option. Note that in opposite as default NETCDF support this format **conserves geometry information**.
+- mysql: export data to a mysql database
+- mysql_speed: export data to a mysql database
+- spread: write data for Excel spreadsheets
+- csv: write in column separated values, another one for spreadsheets
+- text: data in text format
+- bin: data native binary floating point
+- ieee: data in big endian IEEE format
+- ijbox: write a rectangular grid of data
+- AAIG: arcinfo ascii grid, GIS
+
+After conversion each analysis or forecast time will be given in
+separated file replacing the file name TIME information with the hour
+with one leading zero (e.g +0H24H code will be converted to +00H, +01H,
+.. , +25H).
+
+#### CLI usage
+
+TODO
+
+#### API usage
 
 
 ## License
--- cams.py
+++ cams.py
@@ -20,10 +20,12 @@
 
 from enum import Enum
 from os import path
+from subprocess import call
 import datetime
 import argparse
 import requests
 import sys
+
 
 class Param(Enum):
 
@@ -32,8 +34,12 @@ class Param(Enum):
         return (any(value == item.value for item in cls))
 
     @classmethod
-    def get_value(cls, value):
-        return
+    def from_filename(cls, filename):
+        for item in cls:
+            if item.value in filename:
+                return item
+        return None
+
 
 class Model(Param):
     CHIMERE = 'CHIMERE'
@@ -83,11 +89,92 @@ class Time(Param):
     FORECAST_73H96H = '73H96H'
     ALLTIMES = 'ALLTIMES'   # Custom option for download all times (not included in REST API)
 
+    def get_base_time(self):
+        if self == Time.ANALYSIS_24H1H:
+            return -24
+        elif self == Time.FORECAST_0H24H:
+            return -1   # Forecast 0H24 includes 0 hour with base analysis.
+        elif self == Time.FORECAST_25H48H:
+            return 24
+        elif self == Time.FORECAST_49H72H:
+            return 48
+        else:
+            return 72
+
+    def get_hours_range(self):
+        """
+        Get the number of hours available in a time period package. Defacto gets the number of bands in a package.
+        Usually 24 but in the case of Time.FORECAST_0H24H it also include a band with base analysis (time 00)
+        :return: Number of bands available in a time period package.
+        """
+        if self == Time.FORECAST_0H24H:
+            return 25
+        else:
+            return 24
+
+
 
 class Format(Param):
     GRIB2 = 'GRIB2'
     NETCDF = 'NETCDF'
     TXT = 'TXT'
+
+class Wgrib2Format(Param):
+    """
+        -netcdf: write data in netcdf format
+        -mysql: export data to a mysql database
+        -mysql_speed: export data to a mysql database
+        -spread: write data for spreadsheets
+        -csv: write in column separated values, another one for spreadsheets
+        -text: data in text format
+        -bin: data native binary floating point
+        -ieee: data in big endian IEEE format
+        -ijbox: write a rectangular grid of data
+        -AAIG: arcinfo ascii grid, GIS
+    """
+    NETCDF = 'netcdf'
+    MYSQL = 'mysql'
+    MYSQL_SPEED = 'mysql_speed'
+    SPREAD = 'spread'
+    CSV = 'csv'
+    TEXT = 'text'
+    BIN = 'bin'
+    IEEE = 'ieee'
+    IJBOX = 'ijbox'
+    AAIG = 'AAIG'
+
+    def get_file_extension(self):
+        if self == Wgrib2Format.NETCDF:
+            return 'nc'
+        elif self == Wgrib2Format.SPREAD:
+            return 'xls'
+        elif self == Wgrib2Format.CSV:
+            return 'csv'
+        elif self == Wgrib2Format.TEXT:
+            return 'txt'
+        elif self == Wgrib2Format.AAIG:
+            return 'asc'
+        elif self._is_binary_format():
+            return 'bin'
+        elif self._is_db_format():
+            return 'sql'
+        else:
+            return ''
+
+    def to_cmd(self):
+        return '-{0}'.format(self.value)
+
+    def _is_db_format(self):
+        if self == Wgrib2Format.MYSQL or self == Wgrib2Format.MYSQL_SPEED:
+            return True
+        else:
+            return False
+
+    def _is_binary_format(self):
+        if self == Wgrib2Format.BIN or self == Wgrib2Format.IEEE or self == Wgrib2Format.IJBOX:
+            return True
+        else:
+            return False
 
 
 class Downloader:
@@ -116,16 +203,22 @@ class Downloader:
         Note that archived analyses from partner models could be missing, occasionally, in case of production problems. 
         In such a case, please use Ensemble analyses instead.
 
-        :param self: 
         :param out_path: path to the folder where data will be stored.
-        :param reference_time: datetime 
-        :param model: 
-        :param type: 
-        :param species: 
-        :param level: 
-        :param time: 
-        :param data_format: 
-        :return: 
+        :param reference_time: Date of the forecast and analysis production
+        :type reference_time: datetime
+        :param model: Name of one of the available CAMS atmospheric models
+        :type model: Model
+        :param type: One of the available package types for analysis or forecast products
+        :type type: PackageType
+        :param species: One of the available package species (O3, CO, etc.)
+        :type species: PackageSpecies
+        :param level: Surface or all available levels
+        :type level: PackageLevel
+        :param time: One of the available analysis or forecast times. ALL TIMES is not available.
+        :type time: Time
+        :param data_format: Downloaded data output format 
+        :type data_format: Format
+        :return: Path to the downloaded file or None if error
         """
         # TODO check date and package compatibility
         # Referece date is formatted as YYYY-MM-DDT00:00:00Z
@@ -172,6 +265,33 @@ class Downloader:
             elif time != Time.ALLTIMES:
                 # Download just single specie
                 df = Downloader.download(out_path, reference_time, model, package_type, species, level, time, data_format)
+                down_files.append(df)
+        return down_files
+
+class Converter:
+
+    @staticmethod
+    def convert(grib2_in, path_out, format_out=Wgrib2Format.NETCDF):
+        time = Time.from_filename(path.basename(grib2_in))
+        files = []
+        print("Converting {0} to {1}".format(path.basename(grib2_in), format_out.value))
+        for hour in range(1, time.get_hours_range() + 1):
+            hour_num = hour+time.get_base_time()
+            out_filename = grib2_in.replace(time.value, Converter._format_hour(hour_num)).replace('.grib2', '.{0}'.format(format_out.get_file_extension()))
+            out_filepath = path.join(path_out, out_filename)
+            cmd = ['wgrib2', grib2_in, '-d', str(hour), format_out.to_cmd(), out_filepath]
+            # TODO Add error control. Check if wgrib2 worked
+            call(cmd)
+            files.append(out_filepath)
+        return files
+
+    @staticmethod
+    def _format_hour(hour):
+        if -10 < hour < 0 :
+            return '{0}H'.format(hour).replace('-', '-0')
+        else:
+            return '{:02d}H'.format(hour)
+
 
 if __name__ == '__main__':
 
@@ -180,10 +300,11 @@ if __name__ == '__main__':
     parser.add_argument('--type', help='FORECAST|ANALYSIS|ANALYSISREPORT|FORECASTREPORT', default='FORECAST')
     parser.add_argument('--species', help='CO|NH3|NMVOC|NO|NO2|O3|PANS|PM10|PM25|SO2|BIRCHPOLLEN|OLIVEPOLLEN|GRASSPOLLEN|ALLSPECIES(for analysis and forecast reports only)', default='ALLSPECIES')
     parser.add_argument('--level', help='SURFACE|ALLLEVELS', default='SURFACE')
-    parser.add_argument('--time', help='-24H-1H|0H24H|25H48H|49H72H|73H96H', default='ALLTIMES')
+    parser.add_argument('--time', help='_24H_1H|0H24H|25H48H|49H72H|73H96H', default='ALLTIMES')
     parser.add_argument('--reference', help='Reference date of the forecast formatted as yyyy-mm-dd', default=datetime.date.today().strftime("%Y-%m-%d"))
     parser.add_argument('--format', help='GRIB2|NETCDF|TXT', default='GRIB2')
     parser.add_argument('--out', help='Path to the folder where data will be saved', default='./')
+    parser.add_argument('--convert', help='Convert downloaded data with wgrib2 to one of the available formats netcdf|mysql|mysql_speed')
     args = parser.parse_args()
     param_err = ''
     if not Model.has_value(args.model):
@@ -195,10 +316,12 @@ if __name__ == '__main__':
     if not PackageLevel.has_value(args.level):
         param_err += 'Package level, '
     # Check time is valid and compatible with package type
+    args.time = args.time.replace('_', '-')
     if not Time.has_value(args.time):
         param_err += 'Time, '
     if args.time == Time.ANALYSIS_24H1H.value and 'FORECAST' in args.type:
-        param_err += 'Given time is not a forecast, '
+        print("Changed Package type to {0}".format(PackageType.FORECAST.value))
+        args.type = PackageType.ANALYSIS
     # Format
     if not Format.has_value(args.format):
         param_err += 'Format, '
@@ -206,6 +329,8 @@ if __name__ == '__main__':
         reference_date =datetime.datetime.strptime(args.reference, "%Y-%m-%d")
     except ValueError:
         param_err += 'Reference date, '
+    if args.convert and not Wgrib2Format.has_value(args.convert):
+        param_err += "wgrib2 conversion format, "
     # Download if no errors
     if not param_err:
         model = Model(args.model)
@@ -214,13 +339,22 @@ if __name__ == '__main__':
         level = PackageLevel(args.level)
         time = Time(args.time)
         out_format = Format(args.format)
-
+        down_files = []
+        # Download bulk or single data
         if (species == PackageSpecies.ALL_SPECIES or time == Time.ALLTIMES) and args.format != Format.TXT:
-            Downloader.downloadAll(args.out, reference_date, model, species, level, out_format)
+            down_files = Downloader.downloadAll(args.out, reference_date, model, species, level, out_format)
         else:
-            Downloader.download(args.out, reference_date, Model(args.model),
+            df = Downloader.download(args.out, reference_date, Model(args.model),
                                 PackageType(args.type), PackageSpecies(args.species), PackageLevel(args.level),
                                 Time(args.time), Format(args.format))
+            down_files.append(df)
+        # Convert
+        if args.convert and out_format == Format.GRIB2:
+            to_format = Wgrib2Format(args.convert)
+            for df in down_files:
+                Converter.convert(df, args.out, to_format)
+
+
     else:
         print("Error: invalid value for {0}".format(param_err))
         parser.print_help()
