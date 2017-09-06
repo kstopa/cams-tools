6d2a0435a4b12155416b870abcae692c9bc265b6
Krzysztof Stopa <stopa.krzysztof.k@gmail.com>
2017-09-06 07:52:50 +0200

    Added basic setup script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='CAMS tools',
+      version='0.2',
+      description='Copernicus Atmosphere Monitoring Service (CAMS) downloading and processing tools',
+      author='Krzysztof Stopa',
+      author_email='stopa.krzysztof.k@gmail.com',
+      url='https://github.com/kstopa/cams-tools',
+      packages=['cams'],
+      )
